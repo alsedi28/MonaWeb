44ff40786759e08d8b656195bbff5ea214724cdf
alsedi28 <alsedi28@gmail.com>
2020-05-19 17:39:54 +0300

    При нажатии на лого скроллим ленту в начало.

--- Mona/src/components/header/header.jsx
+++ Mona/src/components/header/header.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import styles from './header.module.css';
 import appNameIcon from '../../../public/icons/appNameIcon.jpg';
 
-const Header = ({ externalClass = "" }) => (
-    <header className={`${styles.container} ${externalClass}`}>
-        <div className={styles.centerContainer}>
-            <img className={styles.icon} src={appNameIcon} width="130px" height="40px" />
-        </div>
-    </header>
-);
+const Header = ({ externalClass = "" }) => {
+    let scrollPageUp = () => window.scrollTo(0, 0);
+
+    return (
+        <header className={`${styles.container} ${externalClass}`}>
+            <div className={styles.centerContainer}>
+                <img className={styles.icon} src={appNameIcon} width="130px" height="40px" onClick={scrollPageUp} />
+            </div>
+        </header>
+    )
+};
 
 export default Header;
