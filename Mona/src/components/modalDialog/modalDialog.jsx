dd490f3792dc04461e681deaf43f48ca63f08ed6
alsedi28 <alsedi28@gmail.com>
2020-05-19 13:45:52 +0300

    Закрываем модальное окно при клике вне окна.

--- Mona/src/components/modalDialog/modalDialog.jsx
+++ Mona/src/components/modalDialog/modalDialog.jsx
@@ -4,21 +4,34 @@ import Loader from '../loader/loader';
 import styles from './modalDialog.module.css';
 import closeIcon from '../../../public/icons/close.png';
 
-const ModalDialog = ({ show, isLoading, title, items, clickClose, externalClass = "" }) => (
-    <div className={`${styles.background} ${externalClass}`} style={{ display: show ? "block" : "none" }}>
-        <div className={styles.dialog}>
-            <header>
-                <img src={closeIcon} width="35px" title="Закрыть" onClick={clickClose}/>
-                <p>
-                    {title}
-                </p>
-            </header>
-            <div className={styles.itemsContainer}>
-                <Loader show={isLoading} externalClass={styles.loader}/>
-                {items.map(item => <UserListItem userIcon={item.icon} userLogin={item.login} userName={item.name} />)}
+const ModalDialog = ({ show, isLoading, title, items, clickClose, externalClass = "" }) => {
+    function clickBackground(event) {
+        let target = event.target;
+
+        if (target.closest(".dialog-ev")) {
+            event.stopPropagation();
+            return;
+        }
+
+        clickClose();
+    }
+
+    return (
+        <div className={`${styles.background} ${externalClass}`} style={{ display: show ? "block" : "none" }} onClick={clickBackground}>
+            <div className={`${styles.dialog} dialog-ev`}>
+                <header>
+                    <img src={closeIcon} width="35px" title="Закрыть" onClick={clickClose} />
+                    <p>
+                        {title}
+                    </p>
+                </header>
+                <div className={styles.itemsContainer}>
+                    <Loader show={isLoading} externalClass={styles.loader} />
+                    {items.map(item => <UserListItem userIcon={item.icon} userLogin={item.login} userName={item.name} />)}
+                </div>
             </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default ModalDialog;
