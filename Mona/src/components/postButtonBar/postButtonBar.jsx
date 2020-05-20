95fc7499ec236d65d2274260816e5935e328c1fa
alsedi28 <alsedi28@gmail.com>
2020-05-20 14:54:23 +0300

    Добавил анимацию при клике на "Сердце".

--- Mona/src/components/postButtonBar/postButtonBar.jsx
+++ Mona/src/components/postButtonBar/postButtonBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import styles from './postButtonBar.module.css';
 
@@ -6,11 +6,23 @@ import heartIcon from '../../../public/icons/heart.png';
 import heartFilledIcon from '../../../public/icons/heartFilled.png';
 import commentsIcon from '../../../public/icons/comments.png';
 
-const PostButtonBar = ({ isActiveLike, clickLike, externalClass = "" }) => (
-    <div className={`${styles.container} ${externalClass}`}>
-        <img src={isActiveLike ? heartFilledIcon : heartIcon} className={styles.like} onClick={clickLike} width="30px" />
-        <img src={commentsIcon} width="30px" />
-    </div>
-);
+const PostButtonBar = ({ isActiveLike, clickLike, externalClass = "" }) => {
+    const imgLike = useRef(null);
+
+    function clickLikeWithAnimation() {
+        clickLike();
+
+        // ƒобавл€ем анимацию дл€ картинки и потом снимаем
+        imgLike.current.classList.add(styles.likeClickActive);
+        setTimeout(() => imgLike.current.classList.remove(styles.likeClickActive), 1000);
+    }
+
+    return (
+        <div className={`${styles.container} ${externalClass}`}>
+            <img src={isActiveLike ? heartFilledIcon : heartIcon} className={styles.like} onClick={clickLikeWithAnimation} ref={imgLike} width="30px" />
+            <img src={commentsIcon} width="30px" />
+        </div>
+    );
+};
 
 export default PostButtonBar;
