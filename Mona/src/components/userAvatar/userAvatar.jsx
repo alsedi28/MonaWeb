3741be7af18eba3adbb238084951ae9985079d1e
alsedi28 <alsedi28@gmail.com>
2020-06-03 14:10:57 +0300

    Перешел в PostDetails на использование UserAvatar.

--- Mona/src/components/userAvatar/userAvatar.jsx
+++ Mona/src/components/userAvatar/userAvatar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import styles from './userAvatar.module.css';
+import { getAvatarPath } from '../../helpers/imagePathHelper';
 
-import blankProfileIcon from '../../../public/icons/blankProfileIcon.png';
+import styles from './userAvatar.module.css';
 
 const UserAvatar = ({ avatar, size, withGrayBorder = false, withOrangeBorder = false, externalClass = "" }) => {
     let borderClass = `${withGrayBorder ? styles.grayBorder : ''} ${withOrangeBorder ? styles.orangeBorder : ''}`;
 
     return (
         <div className={`${styles.container} ${externalClass} ${borderClass}`}
-            style={{ background: `url(${avatar ? avatar : blankProfileIcon}) 50% 10% no-repeat`, width: size, height: size }} />
+            style={{ background: `url(${getAvatarPath(avatar)}) 50% 10% no-repeat`, width: size, height: size }} />
     );
 };
 
