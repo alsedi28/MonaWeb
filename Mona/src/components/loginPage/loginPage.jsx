462ab54eab7dd635be35c847a0bbdf4d35f7afd8
Alexander Shoshiashvili <aleksandr.shoshiashvili@gmail.com>
2020-06-06 19:29:19 +0200

    Started refactoring login page

--- Mona/src/components/loginPage/loginPage.jsx
+++ Mona/src/components/loginPage/loginPage.jsx
@@ -26,25 +26,14 @@ const LoginPage = ({ login, showError = false }) => {
             <Header externalClass="header-external" />
             <div className={styles.container}>
                 <div className={styles.formBlock}>
-                    <form>
-                        <div className={styles.formLabel}>
-                            <p>Welcome</p>
-                            <br />
-                            <img src={appIcon} width="60px" />
-                        </div>
-                        <div className={`${styles.inputWrapper} ${styles.loginWrapper}`}>
-                            <input type="text" className={styles.inputText} ref={loginInput} required />
-                            <span className={styles.floatingLabel}>Введите свой никнейм</span>
-                        </div>
-                        <div className={`${styles.inputWrapper} ${styles.passwordWrapper}`}>
-                            <input type="password" className={styles.inputText} ref={passwordInput} required />
-                            <span className={styles.floatingLabel}>Введите пароль</span>
-                        </div>
-                        <div className={styles.error} style={{ display: showError ? "block" : "none" }}>
-                            <p>Имя пользователя или пароль указаны неправильно.</p>
-                        </div>
-                        <button onClick={clickLogin} type="button">Войти</button>
-                    </form>
+                    <div className={styles.formLabel}>
+                        <p>Привет!</p>
+                        <img src={appIcon} width="60px" />
+                    </div>
+
+                    <div className={styles.error} style={{ display: showError ? "block" : "none" }}>
+                        <p>Имя пользователя или пароль указаны неправильно.</p>
+                    </div>
                 </div>
             </div>
             <Footer externalClass="footer-external" />
