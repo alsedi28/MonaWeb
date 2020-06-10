94f2ac755dea3f140bed8d6657c7ed54bb065a97
alsedi28 <alsedi28@gmail.com>
2020-06-10 12:40:31 +0300

    Показываем тень только, если элементов больше 3-х

--- Mona/src/components/horizontalScrollContainer/horizontalScrollContainer.jsx
+++ Mona/src/components/horizontalScrollContainer/horizontalScrollContainer.jsx
@@ -5,7 +5,7 @@ import styles from './horizontalScrollContainer.module.css';
 const HorizontalScrollContainer = ({ title, children, externalClass = "" }) => (
     <section className={`${styles.container} ${externalClass}`}>
         <p>{title}</p>
-        <div>
+        <div className={`${children.length > 3 ? styles.shadow : ''}`}>
             {children}
         </div>
     </section>
